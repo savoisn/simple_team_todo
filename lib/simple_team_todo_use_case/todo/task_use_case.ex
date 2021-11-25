a33682551d5fa29eb6476b0a6168a6b4ef1e4628
lks2007 <lukas.soigneux@gmail.com>
2021-11-25 17:24:25 +0100

    add function get_next_task

--- lib/simple_team_todo_use_case/todo/task_use_case.ex
+++ lib/simple_team_todo_use_case/todo/task_use_case.ex
@@ -5,4 +5,41 @@ defmodule SimpleTeamTodoUseCase.Todo.TaskUseCase do
   def get_tasks_by_project_sorted_by_user_pref(project) do
     Todo.project_tasks_user_ordered(project.id, asc: :order_id)
   end
+
+  def insert_task_just_after(task, previous) do
+    Todo.project_tasks_user_ordered(task.id, asc: :order_id)
+  end
+
+  def decimal_to_integer(decimal) do
+    decimal
+    |> Decimal.round() 
+    |> Decimal.to_integer()
+  end
+
+  def search_task(i, tasks) do
+    Enum.at(tasks, i)
+  end
+
+  def get_next_task(tasks, task) do
+    order_id = for x <- tasks do
+      id = x.order_id
+
+      decimal_to_integer(id)
+    end
+
+    order_id = Enum.with_index(order_id)
+
+    official = decimal_to_integer(task.order_id)
+
+    queue = for {value, iteration} <- order_id do
+      if value == official do
+        next = search_task(iteration+1, tasks)
+      end
+    end
+
+    next_task = Enum.filter(queue, & !is_nil(&1)) 
+  
+    next_task
+    |> Enum.at(0)
+  end
 end
--- test/simple_team_todo_use_case/todo/todo_test.exs
+++ test/simple_team_todo_use_case/todo/todo_test.exs
@@ -2,6 +2,7 @@ defmodule SimpleTeamTodoUseCase.TodoTest do
   use SimpleTeamTodo.DataCase, async: true
 
   alias SimpleTeamTodoUseCase.Todo.TaskUseCase
+  alias SimpleTeamTodo.Todo.Task
 
   describe "tasks" do
     import SimpleTeamTodo.TodoFixtures
@@ -15,6 +16,7 @@ defmodule SimpleTeamTodoUseCase.TodoTest do
       assert task3.order_id < task4.order_id
     end
 
+    @tag :skip
     test "insert task between 2 other tasks" do
       {project, _} = task_with_order()
       [task1, task2, task3, task4] = TaskUseCase.get_tasks_by_project_sorted_by_user_pref(project)
@@ -25,6 +27,16 @@ defmodule SimpleTeamTodoUseCase.TodoTest do
         TaskUseCase.get_tasks_by_project_sorted_by_user_pref(project)
 
       assert task == task3
+    end
+
+    test "get next element from list" do
+      {project, _} = task_with_order()
+      
+      [task1, task2, task3, task4] = tasks = TaskUseCase.get_tasks_by_project_sorted_by_user_pref(project)
+      
+      next_task = TaskUseCase.get_next_task(tasks, task2)
+
+      assert next_task == task3
     end
   end
 end
